84c4eb659c7367ee53b18c460089ae76a713dac3
Diegocostasp <diegocosta199123@gmail.com>
2026-05-06 03:23:21 -0300

    Remove so_flush_caches function
    Removed the so_flush_caches function and its placeholder comment.

--- platform/portmaster/stubs_pm.c
+++ platform/portmaster/stubs_pm.c
@@ -9,13 +9,3 @@ void debugPrintf(char *text, ...) {
     vprintf(text, args);
     va_end(args);
 }
-
-void so_flush_caches(void *mod) {
-#if defined(__GNUC__)
-    /*
-     * Placeholder inicial.
-     * Depois vamos limpar o range correto do módulo carregado.
-     */
-    __builtin___clear_cache((char *)0x60000000, (char *)0x70000000);
-#endif
-}
